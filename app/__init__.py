400e7a1905795a0ed4458a49b548acfc3d5a3913
aline-bebe <ingabirealine40@gmail.com>
2019-09-27 10:50:49 +0200

    installing bcrypt and import it in init

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 # from flask_mail import Mail
 
@@ -12,37 +13,37 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-
-bootstrap = Bootstrap()
-db = SQLAlchemy()
-
-photos = UploadSet('photos', IMAGES)
+photos = UploadSet('photos',IMAGES)
+bcrypt = Bcrypt()
 # mail = Mail()
 
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+
 def create_app(config_name):
-
     app = Flask(__name__)
-
-    #Creating the app configurations
-    app.config.from_object(config_options[config_name])
-
-    # Initializing flask extensions
-    bootstrap.init_app(app)
+    
+    # initializing flask extensions
     db.init_app(app)
     login_manager.init_app(app)
+    bootstrap.init_app(app)
+    bcrypt.init_app(app)
     # mail.init_app(app)
     
-
+    # Creating app configurations
+    app.config.from_object(config_options[config_name])
+    
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
+    
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
-
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    
     # configure UploadSet
     configure_uploads(app,photos)
     
-    # Will add the views and forms
-
+    from .requests import configure_request
+    configure_request(app)
+    
     return app
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,42 +1,47 @@
 
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect,url_for,flash,abort,request
 from ..models import User
-from .forms import RegistrationForm, LoginForm
-from .. import db
+from .forms import RegistrationForm,LoginForm
 from . import auth
-from flask_login import login_user, logout_user, login_required
-# from ..email import mail_message
-
-@auth.route('/login', methods=["GET", "POST"])
-def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user .verify_password(login_form.password.data):
-            login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
-
-        
-    title = "writer LogIn"
-    return render_template('auth/login.html', login_form = login_form, title = title)
-
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("main.index"))
+from .. import db,bcrypt
+from flask_login  import login_user, logout_user, login_required, current_user
 
 @auth.route('/register', methods = ["GET","POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username =form.username.data, password = form.password.data)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email=form.email.data, password = hashed_password)
 
-        db.session.add(user)
-        db.session.commit()
+        user.save_user()
+        flash(f"Account Created for {form.username.data}!","success")
         
-        # mail_message("Welcome to views_pitches","email/welcome_user",user.email,user=user)
+        return redirect(url_for("auth.login"))
+    return render_template('auth/register.html', form = form)
 
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html', registration_form = form)
+
+@auth.route('/login', methods = ["GET","POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            
+            return redirect(url_for('main.index'))
+        else:
+            flash("Login Unsuccessful.Check Email and Password")
+        
+        
+    
+    return render_template('auth/login.html', form = form)
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    
+    return redirect(url_for('main.index'))
