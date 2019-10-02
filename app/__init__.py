82ea884c5f238cbf9ba72c4dc2dc4f8e08362490
aline-bebe <ingabirealine40@gmail.com>
2019-10-02 15:28:23 +0200

    debug

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,13 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-# from flask_bcrypt import Bcrypt
-# from flask_uploads import UploadSet,configure_uploads,IMAGES
-# from flask_mail import Mail
 
 
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-# photos = UploadSet('photos',IMAGES)
-# bcrypt = Bcrypt()
-# mail = Mail()
+
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
@@ -27,8 +22,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     bootstrap.init_app(app)
-    # bcrypt.init_app(app)
-    # mail.init_app(app)
+    
     
     # Creating app configurations
     app.config.from_object(config_options[config_name])
@@ -40,8 +34,7 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
     
-    # configure UploadSet
-    # configure_uploads(app,photos)
+   
     
     from .requests import configure_request
     configure_request(app)
