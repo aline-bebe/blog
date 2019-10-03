3eca9e7930654de581e7c95ddd6d9cac48c80ebd
aline-bebe <ingabirealine40@gmail.com>
2019-10-03 09:15:13 +0200

    deploying

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class TestConfig(Config):
 
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 class DevConfig(Config):
